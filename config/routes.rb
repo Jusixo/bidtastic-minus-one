ef3f5600ce22baa7c128cfc33350703e65ad02ae
Valeria Benetti <vbenetti46@gmail.com>
2017-05-09 13:52:42 -0400

    Added Paths for Auctions to Items

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'pages#landing'
 
-  resources :auctions
+  resources :auctions do
+    resources :items
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
