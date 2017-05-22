0d2e42e7e14f9894e9692bf81893b2fc0e154728
Justin Valentin <Justinvalentine88@gmail.com>
2017-05-22 11:41:27 -0400

    updated landing page for sign in/out links, linked to hosts page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  force_ssl if: :ssl_configured?
+
+  protected
+
+  def ssl_configured?
+    Rails.env.production?
+  end
+
   protect_from_forgery with: :exception
 
   # Assign the current user
